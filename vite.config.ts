8b8232097e450ad49b2da0318a7984183237b308
TULSIRAKHOLIYA077 <radhikarakholiya077@gmail.com>
2025-06-23 18:13:04 +0530

    Updated UI or features

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pos-project/',
+  base: '/pso-project/',
   plugins: [react(), tailwindcss()],
 })
